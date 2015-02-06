9e22e6c101787bdc735ebe56afd1199449a5a9a7
Piotr S. Staszewski <p.staszewski@gmail.com>
2015-02-06 18:47:10 +0100

    Enable assets and nicer logging in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ Huginn::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.js_compressor  = :uglifier
   config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -78,7 +78,7 @@ Huginn::Application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  #config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
